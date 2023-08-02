5e4baaaabfd3b56fde042560d2e526432b6a84cf
KaioSantos00 <kaiosantos.infor2017@gmail.com>
2023-08-02 14:24:47 -0300

    Initial

---
+++ Todo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(max_length=100)
+    date =  models.DateTimeField()
+    done = models.BooleanField(default=False)
+    user  = models.ForeignKey(User, on_delete=models.CASCADE)
