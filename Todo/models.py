b4dec31749bd75ba66e3e947b5c9d2698fac5ada
nalberthdionisio <nalberthdionisio@aluno.fapce.edu.br>
2023-08-02 16:58:38 -0300

    componentização

--- Todo/models.py
+++ Todo/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(max_length=100)
-    date =  models.DateField()
+    date =  models.DateTimeField()
     done = models.BooleanField(default=False)
     user  = models.ForeignKey(User, on_delete=models.CASCADE)
