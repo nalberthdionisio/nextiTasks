06d327b5c6327fcd9a570e02ebd788cdc7da93dc
KaioSantos00 <kaiosantos.infor2017@gmail.com>
2023-08-04 17:21:32 -0300

    project close

--- Todo/urls.py
+++ Todo/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('main/', views.main, name='main'),
+    path('', views.main, name='main'),
     path('item/<int:id>', views.item, name='item'),
     path('formtasks/', views.formtasks, name='formtasks'),
     path("taskdelete/<int:id>", views.taskdelete, name="taskdelete"),
