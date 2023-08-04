37fd702f00795d5e96eb632d209bd0acec1a49ec
nalberthdionisio <nalberthdionisio@aluno.fapce.edu.br>
2023-08-04 16:34:00 -0300

    mini curso finalizado

--- Todo/views.py
+++ Todo/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, HttpResponse,get_object_or_404, redirect
 from .models import *
+from django.contrib.auth.decorators import login_required
 from .forms import *
 # Create your views here.
+@login_required(login_url='/login')
 def main(request):
     tasks = Task.objects.all()
     return render(request, 'main.html',{'tasks':tasks})
-
+@login_required(login_url='/login')
 def item(request, id):
     tasks = Task.objects.all()
 
@@ -14,30 +16,28 @@ def item(request, id):
 
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
-        print(form.errors)
         if form.is_valid():
             form.save()
 
     return render(request, 'item.html',{'task':task, 'tasks':tasks, 'form':form})
-
+@login_required(login_url='/login')
 def formtasks(request):
     tasks = Task.objects.all()
     if request.method == 'POST':
         form = TaskForm(request.POST)
         # form.user = request.user
-        print(form.name)
         if form.is_valid():
             form.save()
             return redirect("/main")
     else:
         form = TaskForm()
     return render(request, 'formtasks.html', {'form': form, 'tasks':tasks})
-
+@login_required(login_url='/login')
 def taskdelete(request,id):
     task = get_object_or_404(Task, id=id)
     task.delete()
     return redirect("/main")
-
+@login_required(login_url='/login')
 def taskedit(request,id):
     task = get_object_or_404(Task, id=id)
     task.delete()
