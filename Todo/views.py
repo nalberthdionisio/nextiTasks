c457773d20e1b709e0eb18b63b7f3402d47173d4
KaioSantos00 <kaiosantos.infor2017@gmail.com>
2023-08-04 16:52:28 -0300

    ajustes finos

--- Todo/views.py
+++ Todo/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, HttpResponse,get_object_or_404, redirect
 from .models import *
-from django.contrib.auth.decorators import login_required
 from .forms import *
 # Create your views here.
-@login_required(login_url='/login')
 def main(request):
     tasks = Task.objects.all()
-    return render(request, 'main.html',{'tasks':tasks})
-@login_required(login_url='/login')
+
+    if request.method == 'POST':
+        post_data = request.POST.copy()
+        post_data.update({'user': request.user})
+        form = TaskForm(post_data)
+        if form.is_valid():
+            form.save()
+            return redirect("/main")
+            # return redirect("/main")
+    else:
+        form = TaskForm()
+    return render(request, 'main.html',{'tasks':tasks,'form': form})
+
 def item(request, id):
     tasks = Task.objects.all()
 
@@ -15,22 +24,21 @@ def item(request, id):
     form = TaskForm(instance=task)
 
     if request.method == 'POST':
-        form = TaskForm(request.POST, instance=task)
+        post_data = request.POST.copy()
+        post_data.update({'user': request.user})
+        form = TaskForm(post_data, instance=task)
+        print(form.errors)
         if form.is_valid():
             form.save()
 
     return render(request, 'item.html',{'task':task, 'tasks':tasks, 'form':form})
-@login_required(login_url='/login')
+
 def formtasks(request):
     tasks = Task.objects.all()
     if request.method == 'POST':
-
-
         post_data = request.POST.copy()
         post_data.update({'user': request.user})
         form = TaskForm(post_data)
-        
-
         if form.is_valid():
             form.save()
             return redirect("/main")
@@ -38,12 +46,12 @@ def formtasks(request):
     else:
         form = TaskForm()
     return render(request, 'formtasks.html', {'form': form, 'tasks':tasks})
-@login_required(login_url='/login')
+
 def taskdelete(request,id):
     task = get_object_or_404(Task, id=id)
     task.delete()
     return redirect("/main")
-@login_required(login_url='/login')
+
 def taskedit(request,id):
     task = get_object_or_404(Task, id=id)
     task.delete()
